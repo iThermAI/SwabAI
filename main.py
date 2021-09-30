c2b9d9d755491550f04b2447fd442b9f6b9164b3
alient12 <ali_ent12@outlook.com>
2021-09-30 10:15:13 +0330

    changed frame drop condition

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ import cv2
 
 img = cv2.imread("input.jpg")  # img is numpy array
 
-points = swab_ai.Get_BoundingBox(img)  # returns points of bounding box
+points = swab_ai.Get_BoundingBox(img)  # returns bounding box coordinates of img
 
-# check if points exist
-if isinstance(points, int):
-    if points == 0:
-        # No face or mouth, No coordinations output
-        print("can not detect mouth")
-else:
+if points:  # bounding box is found
     # save output file -> only for debugging purposes
     cv2.imwrite("output.jpg", cv2.rectangle(img, points[0], points[1], (0, 0, 255), 2))
     print("output.jpg is written.")
